990fb1023e5df5da89f91282d78e355263d9d93a
Unknown <clayton.brez@gmail.com>
2018-02-19 16:53:47 -0600

    price data pulling from database being displayed on highstocks

--- glucosetracker/coins/views.py
+++ glucosetracker/coins/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+import json
+from django.shortcuts import render, HttpResponse
 from .models import Cryptocurrency, Coin, Cryptocurrencytable
-
 from django_tables2 import RequestConfig
-
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from .reports import ChartData
 
 
 # Create your views here.
@@ -30,3 +30,12 @@ def cryptocurrenciesView(request):
 def coinsChartDataJson(request):
     data = {}
     params = request.GET
+    # to pull from the js code from cryptocurrencies.html, use this method. If nothing is added, days = 0, name = ""
+    # days = params.get('days', 0)
+    # name = params.get('name', '')
+
+    # TODO let currency choice get pulled from the webpage
+    # data will contain crypto name, price, and the unix time
+    data['chart_data'] = ChartData.getCurrencyData(currency='Bitcoin')
+
+    return HttpResponse(json.dumps(data), content_type='application/json')
