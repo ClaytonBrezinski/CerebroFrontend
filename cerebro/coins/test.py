1fe5a796c8ccca0f43cd92765a76acc5bb24f59d
Unknown <clayton.brez@gmail.com>
2018-04-13 14:27:55 -0600

    add test files from local development

--- cerebro/coins/test.py
+++ cerebro/coins/test.py
@@ -17,13 +17,13 @@ class CryptocurrencyModelTestCase(TestCase):
     This is a test suite for the Cryptocurrency model
     """
 
-    def setUp(self):
+    def setUp(self, cryptocurrencyName='test', cryptocurrencyTickerSymbol='tst'):
         """
         Define variables for a basic cryptocurrency
         :return:
         """
-        self.cryptocurrencyName = 'test'
-        self.cryptocurrencyTickerSymbol = 'tst'
+        self.cryptocurrencyName = cryptocurrencyName
+        self.cryptocurrencyTickerSymbol = cryptocurrencyTickerSymbol
         self.cryptocurrency = Cryptocurrency(name=self.cryptocurrencyName, tickerSymbol=self.cryptocurrencyTickerSymbol)
 
     def testCreateNewModel(self):
@@ -32,7 +32,9 @@ class CryptocurrencyModelTestCase(TestCase):
         :return:
         """
         oldCount = Cryptocurrency.objects.count()
-        self.cryptocurrency.save()
+        # self.cryptocurrency.save()
+        currency = Cryptocurrency.objects.create(Cryptocurrency='BTC', Price=1234, Volume=12, Time='')
+        currency.save()
         newCount = Cryptocurrency.objects.count()
         self.assertNotEqual(oldCount, newCount)
 
@@ -41,7 +43,8 @@ class CryptocurrencyModelTestCase(TestCase):
         Create cryptocurrencies with errornous data inputs
         :return:
         """
-        pass
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
+        self.client.post({Name = '', tickerSymbol = 123456, Active = 'QQ'})
 
 
 class CoinModelTestCase(TestCase):
@@ -81,12 +84,16 @@ class CoinModelTestCase(TestCase):
         Create coins with errornous data inputs
         :return:
         """
-        pass
+        currency = Cryptocurrency.objects.create(Name='', tickerSymbol=123456, Active='QQ')
+        self.assertFormError(self.post.currency)
+        coin = Coin.objects.create(Cryptocurrency='', Price='qq', Volume='qq', Time='123')
+        self.assertFormError(self.post.coin)
 
 
 class CryptoCurrencyViewTestCase(TestCase):
     """
-    Test suite for the API views
+    Test suite for the API views for cryptocurrencies
+
     """
 
     def setUp(self):
@@ -99,32 +106,48 @@ class CryptoCurrencyViewTestCase(TestCase):
         self.client.force_authenticate(user=testUser)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
 
-        # self.cryptocurrencyData = {'name': "test", 'tickerSymbol': "tst"}
-        #
-        # self.response = self.client.post(
-        #         reverse('coinAPI'),
-        #         self.cryptocurrencyData,
-        #         format="json")
+        self.cryptocurrencyData = {'name': "test", 'tickerSymbol': "tst"}
 
-    def testAPIput(self):
+        self.response = self.client.post(
+                reverse('coinAPI'),
+                self.cryptocurrencyData,
+                format="json")
+
+    def testAPInput(self):
         """
         test that the api can create a new model
         :return:
         """
-        # self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
-        pass
 
 
 class CoinViewTestCase(TestCase):
     """
-    Test suite for the API views
+    Test suite for the API views for coins
     """
 
     def setUp(self):
-        pass
+        import requests
+        self.url = 'http://127.0.0.1:8000'
+        self.apiToken = requests.post('http://127.0.0.1:8000/api-token-auth/', params={
+            'username': 'admin', 'password': 'pass'})
 
-    def testAPIput(self):
+    def testAPInput(self):
         """
         test that the api can create a new model
         :return:
         """
+        # test cryptocurrency
+        import requests
+        from datetime import datetime
+        useURL = self.url + '/coins/cryptocurrencyAPI'
+        requests.post(useURL,
+                      params={'name': 'Bitcoin', 'tickerSymbol': 'BTC', 'active': True,
+                              'Authorization': 'Token %s' % self.apiToken})
+
+        # test coins
+        useURL = self.url + '/coins/coinAPI'
+
+        requests.post(useURL,
+                      params={'cryptocurrency': "cryptocurrency_name='Bitcoin'",
+                              'price': 5, 'volume': 5 'time': datetime.now(),
+                              'Authorization': 'Token %s' % self.apiToken})
